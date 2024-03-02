96b5e2255a8b6b0b245eb5665bca7f6845ffe63d
Sadanand Gopal <sadanandgpl@gmail.com>
2024-03-03 00:48:56 +0530

    init

---
+++ client/src/components/main-chat.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Grid, Typography, TextField, Button, Paper, List, ListItem, ListItemButton, ListItemText, Stack } from '@mui/material';
+
+let isMounted = false;
+const ChatPage = ({ usersList, socket, username }) => {
+	const [selectedUser, setSelectedUser] = useState('');
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		if (socket && !isMounted) {
+			socket.on('new_message', (data) => {
+				console.log(data, 'new-message')
+				setMessages((prevMessages) => [...prevMessages, data,]);
+			});
+			isMounted = true;
+		}
+	}, [socket]);
+
+
+	const handleMessageChange = (event) => {
+		setMessage(event.target.value);
+	};
+
+	const handleSendMessage = () => {
+		if (message.trim() === '') {
+			toast.error('Please enter a message!');
+		} else if (!selectedUser) {
+			toast.error('Please select a user to send the message!');
+		} else {
+			const sender = username;
+			socket.emit('send_message', { sender, receiver: selectedUser, message });
+			setMessages([...messages, { sender, message }]);
+			setMessage('');
+		}
+	};
+
+	const handleUserSelect = (user) => {
+		setSelectedUser(user);
+		toast.success(`Selected user: ${user}`);
+	};
+
+	return (
+
+		<Grid container spacing={2}>
+			<Grid item xs={4}>
+				<Typography variant='subtitle1' color='primary'>Logged In {username}</Typography>
+				<Typography variant="subtitle2">Joined Users</Typography>
+				<List component={Paper} dense sx={{ p: 2, minHeight: 400 }}>
+					{usersList.filter(user => user !== username).map((user) => (
+						<ListItem sx={{ borderBottom: '1px solid', borderColor: 'divider' }} disablePadding key={user} >
+							<ListItemButton selected={user === selectedUser} onClick={() => handleUserSelect(user)}>
+								<ListItemText primary={user} />
+							</ListItemButton>
+						</ListItem>
+					))}
+
+				</List>
+
+			</Grid>
+			<Grid item xs={8}>
+				{/* Right side: Message content */}
+				<Typography variant="h6" mb={2}>Chat {selectedUser}</Typography>
+				<Stack component={Paper} style={{ overflowY: 'auto', padding: '8px', minHeight: 400, }}>
+
+					<Stack sx={{ flex: 1, marginTop: 'auto' }}>
+						{messages.map((msg, index) => (
+							<div key={index} style={{ marginBlock: 8, textAlign: msg.sender === username ? 'right' : 'left', }}>
+								<Paper elevation={1} style={{ backgroundColor: msg.sender === username ? '#d8f4ff' : '#ebebeb', maxWidth: 200, display: 'inline-block', padding: "4px 16px" }}>
+									<Typography variant="body1">
+										<strong>{msg.sender}:</strong> {msg.message}
+									</Typography>
+								</Paper>
+							</div>
+						))}
+					</Stack>
+					<Stack px={2} borderRadius={2} direction='row' alignItems='center' border='1px solid' borderColor="divider">
+						<TextField
+							variant="standard"
+							margin="normal"
+							fullWidth
+							id="message"
+							name="message"
+							value={message}
+							onChange={handleMessageChange}
+						/>
+						{/* Send message button */}
+						<Button
+							type="button"
+							variant="contained"
+							color="primary"
+							onClick={handleSendMessage}
+						>
+							Send
+						</Button>
+					</Stack>
+
+				</Stack>
+				{/* Message input field */}
+
+
+			</Grid>
+		</Grid >
+
+	);
+};
+
+export default ChatPage;
---
+++ server/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("join", (username) => {
+    console.log("joined", username);
+    if (!username || users[username]) {
+      socket.emit("join_error", "Username already taken");
+    } else {
+      users[username] = socket.id;
+      io.emit("users_list", Object.keys(users)); // Send updated users list to all clients
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const username = Object.keys(users).find((key) => users[key] === socket.id);
+    if (username) {
+      delete users[username];
+      io.emit("user_left", username);
+      io.emit("users_list", Object.keys(users)); // Send updated users list to all clients
+    }
+    console.log("A user disconnected");
+  });
+
+  socket.on("send_message", ({ sender, receiver, message }) => {
+    console.log(sender, receiver, message, "socket");
+    const receiverId = users[receiver];
+    if (receiverId) {
+      io.to(receiverId).emit("new_message", { sender, message }); // Send message only to the intended recipient
+    } else {
+      // Handle case where receiver is not found (e.g., user left the chat)
+      console.log(`User ${receiver} not found.`);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
